f80b44edabd2f7bc487f2ce15646e47eb8e492d1
zhang677 <ZhangghLCN@163.com>
2022-07-14 07:58:56 +0000

    Add cuSPARSE comparison

--- test-cg/src/count.py
+++ test-cg/src/count.py
@@ -10,20 +10,24 @@ lines = df.shape[0]
 #actions = 60
 dataset_num = (int)(lines/actions)
 min_actions = []
-datasets = []
 ratios = []
+ratios_cu = []
 min_datasets = {}
 for i in range(actions):
     min_datasets[i] = []
 for i in range(dataset_num):
     times = []
+    cuspt = 0
     for l in range(actions):
-        if (l!=0) and (l!=1) and (l!=2) : # Depends on where cusparse results are recorded
+        if (l<3):
+            cuspt += df.loc[i*actions+l].Time
+        else : # Depends on where cusparse results are recorded
             times.append(df.loc[i*actions+l].Time)
     min_action = times.index(min(times))+3
     min_actions.append(min_action)
-    min_datasets[min_action].append(df.loc[i*actions].dataset)
+    #min_datasets[min_action].append(df.loc[i*actions].dataset)
     ratios.append(times[0]/min(times))
+    ratios_cu.append(cuspt/min(times))
 hist = []
 for i in range(actions):
     hist.append(min_actions.count(i))
@@ -31,3 +35,6 @@ print(hist)
 print(np.mean(ratios))
 print(np.std(ratios))
 print(gmean(ratios))
+print(np.mean(ratios_cu))
+print(np.std(ratios_cu))
+print(gmean(ratios_cu))
