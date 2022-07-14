06247daa8b0ea775ef6ccf6b75659d6cadfcb116
zhang677 <ZhangghLCN@163.com>
2022-07-14 12:17:22 +0000

    Add dynamic static

--- test-cg/src/count.py
+++ test-cg/src/count.py
@@ -9,32 +9,34 @@ df = pd.read_csv(fileName)
 lines = df.shape[0]
 #actions = 60
 dataset_num = (int)(lines/actions)
-min_actions = []
-ratios = []
-ratios_cu = []
-min_datasets = {}
-for i in range(actions):
-    min_datasets[i] = []
-for i in range(dataset_num):
-    times = []
-    cuspt = 0
-    for l in range(actions):
-        if (l<3):
-            cuspt += df.loc[i*actions+l].Time
-        else : # Depends on where cusparse results are recorded
-            times.append(df.loc[i*actions+l].Time)
-    min_action = times.index(min(times))+3
-    min_actions.append(min_action)
-    #min_datasets[min_action].append(df.loc[i*actions].dataset)
-    ratios.append(times[0]/min(times))
-    ratios_cu.append(cuspt/min(times))
-hist = []
-for i in range(actions):
-    hist.append(min_actions.count(i))
-print(hist)
-print(np.mean(ratios))
-print(np.std(ratios))
-print(gmean(ratios))
-print(np.mean(ratios_cu))
-print(np.std(ratios_cu))
-print(gmean(ratios_cu))
+for k in range(actions-3):
+    #min_actions = []
+    ratios = []
+    #ratios_cu = []
+    min_datasets = {}
+    for i in range(actions):
+        min_datasets[i] = []
+    for i in range(dataset_num):
+        times = []
+        cuspt = 0
+        for l in range(actions):
+            if (l<3):
+                cuspt += df.loc[i*actions+l].Time
+            else : # Depends on where cusparse results are recorded
+                times.append(df.loc[i*actions+l].Time)
+        #min_action = times.index(min(times))+3
+        #min_actions.append(min_action)
+        #min_datasets[min_action].append(df.loc[i*actions].dataset)
+        ratios.append(times[k]/min(times))
+        #ratios_cu.append(cuspt/min(times))
+    #hist = []
+    #for i in range(actions):
+    #    hist.append(min_actions.count(i))
+    #print(hist)
+    print(k)
+    print(np.mean(ratios))
+    print(np.std(ratios))
+    print(gmean(ratios))
+    #print(np.mean(ratios_cu))
+    #print(np.std(ratios_cu))
+    #print(gmean(ratios_cu))
