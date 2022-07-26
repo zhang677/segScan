c53b7dac28ca65c0a8d17b4fc88f6e613e0f17b0
zhang677 <ZhangghLCN@163.com>
2022-07-26 07:08:15 +0000

    Fix tuning results

--- test-cg/src/run.sh
+++ test-cg/src/run.sh
@@ -4,6 +4,5 @@ MATRICES_DIR=/home/nfs_data/datasets/sparse_mat
 #MATRICES_DIR=/home/nfs_data/zhanggh/segScan/test-cg/data
 MODE=1
 RESULTS_DIR=/home/nfs_data/zhanggh/segScan/test-cg/profile
-HARDWARE=2080
 export LD_LIBRARY_PATH="/home/eva_share/opt/cuda-11.6/lib64:$LD_LIBRARY_PATH"
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=4 ./test.py $MATRICES_DIR $MODE $RESULTS_DIR $HARDWARE $1
+CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=4 ./test.py $MATRICES_DIR $MODE $RESULTS_DIR $1 $2
