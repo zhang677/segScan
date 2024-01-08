c8085a9a4c9283c0bae055e1ce2d37387a7d41d3
zhang677 <ZhangghLCN@163.com>
2024-01-07 18:46:33 -0800

    Cleaner block configuration

--- new-cg/include/segscan/segscan.cuh
+++ new-cg/include/segscan/segscan.cuh
@@ -182,3 +182,174 @@ void segment_coo(const ValueType* src, const IndexType* index, const int nnz, co
     gpuErrchk(cudaGetLastError());
 
 }
+
+/// Update workload distribution
+template <typename ValueType, typename IndexType, typename AccessType, int group_size>
+__global__ void segscan_new_kernel(const ValueType* src, const IndexType* index, const int nnz, const int N, ValueType* dst) {
+    constexpr int CoarsenFactor = sizeof(AccessType) / sizeof(ValueType);
+    int lane_id = threadIdx.x;
+    int nz_start = blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    int col_offset = (blockIdx.y * blockDim.y + threadIdx.y) * CoarsenFactor;
+    const ValueType *src_panel = src + col_offset;
+    ValueType *dst_panel = dst + col_offset;
+    const int ldsrc = N;
+    const int lddst = N;
+
+    IndexType k;
+    ValueType v;
+    ValueType o[CoarsenFactor] = {0};
+    thread_block_tile<group_size,thread_block> group = tiled_partition<group_size>(this_thread_block());
+
+    if (col_offset >= N) return;    
+    if (col_offset + CoarsenFactor >= N) goto Ndim_Residue;
+
+    for (int nz_id = nz_start + lane_id; nz_id < nnz + lane_id; nz_id += stride) {
+        IndexType row = index[nz_id];
+        if (nz_id < nnz) {
+            k = nz_id; // Feature is sorted
+            v = (ValueType)1; // csr_data is set to 1
+        } else {
+            k = nnz - 1;
+            v = (ValueType)0;
+        }
+
+        // load B-elements in vector-type
+        *(AccessType *)o = *(AccessType *)(src_panel + k * ldsrc);
+        #pragma unroll
+        for (int i = 0; i < CoarsenFactor; i++) {
+            o[i] *= v;
+        }
+
+        int row_intv = group.shfl(row, group.size() - 1) - group.shfl(row, 0);
+        if (row_intv == 0) {
+            #pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                for (int k = group.size() >> 1; k > 0; k >>= 1) {
+                    o[i] += group.shfl_down(o[i], k);
+                }
+            } 
+            if (group.thread_rank() == 0) {
+                #pragma unroll
+                for (int i = 0; i < CoarsenFactor; i++) {
+                    atomicAdd(dst_panel + row * lddst + i, o[i]);
+                }
+            } 
+        } else {
+            bool is_seg_start = ((group.shfl_up(row,1) != row)|| (group.thread_rank() == 0));
+            ValueType tmpv;
+            IndexType tmpr;
+            #pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                for (k = 1; k<group.size(); k = k<<1) {
+                    tmpv = group.shfl_down(o[i],k);
+                    tmpr = group.shfl_down(row,k);
+                    if (tmpr == row && group.thread_rank() < (group.size()-k)) {
+                        o[i] += tmpv;
+                    }
+                }
+            }
+            if (is_seg_start) {
+                #pragma unroll
+                for (int i = 0; i < CoarsenFactor; i++) {
+                    atomicAdd(dst_panel + row * lddst + i, o[i]);
+                }
+            }
+        }
+     }
+     return;
+
+     Ndim_Residue:
+     int valid_lane_num = N - col_offset;
+
+     for (int nz_id = nz_start + lane_id; nz_id < nnz + lane_id; nz_id += stride) {
+        IndexType row = index[nz_id];
+        if (nz_id < nnz) {
+            k = nz_id; // Feature is sorted
+            v = (ValueType)1;
+        } else {
+            k = 0;
+            v = (ValueType)0;
+        }
+
+        // load B-elements in vector-type
+        #pragma unroll
+        for (int i = 0; i < CoarsenFactor; i++) {
+            if (i < valid_lane_num) {
+                o[i] = src_panel[k * ldsrc + i] * v;
+            }
+        }
+
+        int row_intv = group.shfl(row, group.size() - 1) - group.shfl(row, 0);
+        if (row_intv == 0) {
+            #pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                for (int k = group.size() >> 1; k > 0; k >>= 1) {
+                    o[i] += group.shfl_down(o[i], k);
+                }
+            } 
+            if (group.thread_rank() == 0) {
+                #pragma unroll
+                for (int i = 0; i < CoarsenFactor; i++) {
+                    if (i < valid_lane_num) {
+                        atomicAdd(dst_panel + row * lddst + i, o[i]);
+                    }
+                }
+            } 
+        } else {
+            bool is_seg_start = ((group.shfl_up(row,1) != row)|| (group.thread_rank() == 0));
+            ValueType tmpv;
+            IndexType tmpr;
+            #pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                for (k = 1; k<group.size(); k = k<<1) {
+                    tmpv = group.shfl_down(o[i],k);
+                    tmpr = group.shfl_down(row,k);
+                    if (tmpr == row && group.thread_rank() < (group.size()-k)) {
+                        o[i] += tmpv;
+                    }
+                }
+            }
+            if (is_seg_start) {
+                #pragma unroll
+                for (int i = 0; i < CoarsenFactor; i++) {
+                    if (i < valid_lane_num) {
+                        atomicAdd(dst_panel + row * lddst + i, o[i]);
+                    }
+                }
+            }
+        }
+     }
+     return;
+
+}
+
+template <typename ValueType, typename IndexType, int group_factor, int thread_per_block, int tile_factor, int blockDimY>
+void segment_coo_new(const ValueType* src, const IndexType* index, const int nnz, const int N, const int dst_len, ValueType* dst){
+    int coarsen_factor = (N % 4 == 0) ? 4 : (N % 2 == 0) ? 2 : 1;
+    // group_size <= tile_size
+    int tile_size = 1<<tile_factor;
+    int max_blockDimY = thread_per_block / tile_size;
+    int numTileY = CEIL(N, coarsen_factor);
+    int real_blockDimY = blockDimY < max_blockDimY ? MIN(numTileY, blockDimY) : MIN(numTileY, max_blockDimY);
+    int real_blockDimX = CEIL(thread_per_block, real_blockDimY);
+    dim3 gridDim(CEIL(nnz, real_blockDimX), CEIL(numTileY, real_blockDimY), 1);
+    dim3 blockDim(real_blockDimX, real_blockDimY, 1);
+
+    if (coarsen_factor == 4) {
+        segscan_new_kernel<ValueType, IndexType, float4, 1<<group_factor ><<<gridDim, blockDim>>>(
+            src, index, nnz, N, dst);
+    } else if (coarsen_factor == 2) {
+        segscan_new_kernel<ValueType, IndexType, float2, 1<<group_factor ><<<gridDim, blockDim>>>(
+            src, index, nnz, N, dst);
+    } else {
+        segscan_new_kernel<ValueType, IndexType, float, 1<<group_factor ><<<gridDim, blockDim>>>(
+            src, index, nnz, N, dst);
+    }
+
+    cudaDeviceSynchronize();
+    std::cout<<"("<<gridDim.x<<","<<gridDim.y<<","<<gridDim.z<<")"<<std::endl;
+    std::cout<<"("<<blockDim.x<<","<<blockDim.y<<","<<blockDim.z<<")"<<std::endl;  
+    gpuErrchk(cudaGetLastError());
+
+}
